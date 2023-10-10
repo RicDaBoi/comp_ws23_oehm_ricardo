a406fc6733021f16a8804432746829d430e2b794
RicDaBoi <146155920+RicDaBoi@users.noreply.github.com>
2023-10-10 15:14:50 +0200

    Edit String 01

--- profile_converter.py
+++ profile_converter.py
@@ -7,6 +7,13 @@ dest=('output.json')                                                        #cre
 with open('example.json') as inputfile:
     inputdata = json.load(inputfile)                                        #import data from input-file
 
+for x in inputdata:
+    if x[0]:
+        y = 'SusWha'
+        
+
+
+
 with open('output.json', 'w') as outputfile:                            
     json.dump(inputdata, outputfile, ensure_ascii=False, indent=4)          #write data to output-file
 
