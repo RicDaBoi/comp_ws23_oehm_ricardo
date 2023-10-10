756b555e05eec1eef0f139c7b150d47e58e12a77
RicDaBoi <146155920+RicDaBoi@users.noreply.github.com>
2023-10-10 14:55:23 +0200

    Output JSON-file

--- profile_converter.py
+++ profile_converter.py
@@ -1,9 +1,27 @@
-import json
+import shutil
+import json                                                                 #import libraries
 
-inputfile = open('example.json')
-inputdata = json.load(inputfile)
+src=('example.json')
+dest=('output.json')                                                        #create output-file
 
-for x in inputdata:
-    print(x)
+with open('example.json') as inputfile:
+    inputdata = json.load(inputfile)                                        #import data from input-file
 
-inputfile.close()
+with open('output.json', 'w') as outputfile:                            
+    json.dump(inputdata, outputfile, ensure_ascii=False, indent=4)          #write data to output-file
+
+
+
+
+
+
+
+
+
+
+
+
+
+#print(inputdata)
+#for x in inputdata:
+    #print(x)
